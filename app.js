02297016259024b77ee412f4def39e868cd1d690
Samuel <ososeelliot@gmail.com>
2026-01-31 12:59:13 +0100

    add this changes

---
+++ app.js
@@ -0,0 +1,45 @@
+//console.log("App started");
+
+///console.log("Productivity App is loading...");
+
+//console.log("User data loaded");
+
+//console.log("App ready");
+
+//var oldWay = "danger";
+
+//const appName = "Productivity App";
+
+//let userName = "elliot";
+
+const appNam = "Productivity App";
+
+//let tasks = [];
+//let completedTasks = "0";
+
+console.log(appName);
+console.log(tasks);
+console.log(completedTasks);
+
+//const appName = "Tasks app"; //
+
+//let userNumber = "1"; // numbers are used without ""
+
+//console.log(appName, userNumber);
+
+// here is the correct one
+const appName = "Tasks App";
+
+let tasks = [];
+let completedTasks = 0;
+let userNumber = 1;
+
+console.log(appName);
+console.log(tasks);
+console.log(completedTasks);
+
+completedTasks = 1;
+console.log("Completed tasks:", completedTasks);
+
+// SO NOW MOVING TO DATATYPES
+Number,String, Boolean, null, undefined
